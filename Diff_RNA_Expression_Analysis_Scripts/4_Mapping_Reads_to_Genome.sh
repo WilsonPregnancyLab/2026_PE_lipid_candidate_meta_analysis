865645cda1c3613e0041c56b18857b69522b18f2
Kriesha Eyer <168942192+krieshaeyer@users.noreply.github.com>
2025-08-01 16:12:17 -0400

    Update 4_Mapping_Reads_to_Genome.sh

--- Diff_RNA_Expression_Analysis_Scripts/4_Mapping_Reads_to_Genome.sh
+++ Diff_RNA_Expression_Analysis_Scripts/4_Mapping_Reads_to_Genome.sh
@@ -49,18 +49,27 @@ mkdir ./genomeInd
 STAR --runMode genomeGenerate --genomeDir ./genomeInd --genomeFastaFiles ./GRCh38.primary_assembly.genome.fa --sjdbGTFfile ./gencode.v48.primary_assembly.annotation.gtf --runThreadN 4
 
 
-
 #Step 3: Mapping Reads to Genome
 
 ##Create new folder to save mapped files
-mkdir /path_for_mapped_files/
+mkdir ./mapped_reads/mapped_PE/
+mkdir ./mapped_reads/mapped_SE/
+TRIM_DIR="/workspace/lab/wilsonslab/eyerk/2025_RNA_Lipid_Candidate/quality_control/FastP_Trimming"
 
-for file in /path_of_trimmed_fastqs/*.fastq; do
-STAR --runThreadN 4 --genomeDir ./genomeInd --readFilesIn ${file} --outSAMtype BAM SortedByCoordinate --outFileNamePrefix /path_for_mapped_files/${file}; done
+PE_trimmed_1=("$TRIM_DIR"/*_1.fastq)
+PE_trimmed_2=("$TRIM_DIR"/*_2.fastq)
+
+parallel -j 4 --link ' base=$(basename {1} _1.fastq); STAR --runThreadN 4 --genomeDir ./genomeInd --readFilesIn {1} {2} --outSAMtype BAM SortedByCoordinate --outFileNamePrefix ./mapped_reads/mapped_PE/${base}_ ' ::: "${PE_trimmed_1[@]}" ::: "${PE_trimmed_2[@]}"
+
+for file in "$TRIM_DIR"/SE_reads/*.fastq; do
+base=$(basename "$file" .fastq) STAR --runThreadN 4 --genomeDir ./genomeInd --readFilesIn "$file" --outSAMtype BAM SortedByCoordinate --outFileNamePrefix ./mapped_reads/mapped_SE/${file} done
 
 ##Create new folder to transfer just the BAM files
-mkdir /path_for_bam_files/
-mv /path_for_mappped_files/*.bam /path_for_bam_files/
+mkdir ./mapped_BAMs/PE_mapped_BAMs/
+mkdir ./mapped_BAMs/SE_mapped_BAMs/
+mv ./mapped_reads/mapped_PE/*.bam ./mapped_BAMs/PE_mapped_BAMs/
+mv ./mapped_reads/mapped_SE/*.bam ./mapped_BAMs/SE_mapped_BAMs/
+
 
 
 
