428fdb47830f39b3fa9bb7e4fcf4178bd5c6261c
Kriesha Eyer <168942192+krieshaeyer@users.noreply.github.com>
2025-09-19 15:06:03 -0400

    Update 5_Read_Quantification_and_Sex_Prediction.sh

--- Diff_RNA_Expression_Analysis_Scripts/5_Read_Quantification_and_Sex_Prediction.sh
+++ Diff_RNA_Expression_Analysis_Scripts/5_Read_Quantification_and_Sex_Prediction.sh
@@ -10,7 +10,7 @@ cd /workspace/lab/wilsonslab/eyerk/featureCounts
 ##Install directory is the untarred folder after you download
 
 ##Download the featureCounts
-wget http://sourceforge.net/projects/subread/files/subread-2.1.1/
+wget https://sourceforge.net/projects/subread/files/subread-2.1.1/subread-2.1.1-source.tar.gz/
 
 ##Extract Program
 tar -zxvf subread-2.1.1.tar.gz
@@ -18,12 +18,8 @@ tar -zxvf subread-2.1.1.tar.gz
 ##Move into extracted folder
 cd subread-2.1.1
 
-##Configure into installation directory (if binary is not already available)
-cd featureCounts/source
-make featureCounts
-
 ##Add the 'binaries (bin)' folder to yout PATH in .bashrc to allow command to run in any directory
-echo 'export PATH="/workspace/lab/wilsonslab/eyerk/programs/STAR/STAR-2.7.11b/source:$PATH"' >> ~/.bashrc
+echo 'export PATH="/workspace/lab/wilsonslab/eyerk/programs/subread/subread-2.1.1-Linux-x86_64/bin:$PATH"' >> ~/.bashrc
 ##Reload bash
 source ~/.bashrc
 
@@ -36,9 +32,6 @@ cd /workspace/lab/wilsonslab/eyerk/2025_RNA_Lipid_Candidate/genome_mapping/
 
 mkdir ./read_counts 
 
-#SE reads
-featureCounts -a ./gencode.v48.primary_assembly.annotation.gtf -o ./read_counts -T 4 ./mapped_BAMs/SE_mapped_BAMs/*.bam
-# PE reads
 featureCounts -a ./gencode.v48.primary_assembly.annotation.gtf -p --countReadPairs -o ./read_counts.csv -T 4 ./mapped_BAMs/PE_mapped_BAMs/*.bam
 
 
